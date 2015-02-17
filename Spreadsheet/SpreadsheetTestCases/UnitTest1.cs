e3339beb27a913ae05d9dc54ebaa56c690f1eb53
Ali Momeni <aymomeni@gmail.com>
2015-02-16 18:13:29 -0700

    Added test class, added abstract methods

---
+++ Spreadsheet/SpreadsheetTestCases/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spreadsheet;
+
+namespace SpreadsheetTestCases
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
